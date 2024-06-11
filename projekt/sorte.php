79ac1f6c04d8eef7bb71307b6b6f9db425519501
Marko Slovic <113772949+slovicMarko@users.noreply.github.com>
2024-06-11 15:45:07 +0200

    Update sorte.php

--- projekt/sorte.php
+++ projekt/sorte.php
@@ -19,7 +19,7 @@
             $vina = dohvatiVina();
 
             foreach ($vina as $index => $vino) {
-                echo '<a href="article_sorta.php?index=' . $index . '&naziv=' . urlencode($vino["naziv"]) . '&image=' . urlencode($vino["slika"]) . '&kratkiOpis=' . urlencode($vino["kratkiOpis"]) . '&duziOpis=' . urlencode($vino["duziOpis"]) . '&boja=' . urlencode($vino["boja"]) . '&slatkoca=' . urlencode($vino["slatkoca"]) . '&godinaBerbe=' . urlencode($vino["godinaBerbe"]) . '&preporucenaJela=' . urlencode($vino["preporucenaJela"]) . '&date=' . urlencode($vino["datumObjavljivanja"]) . '">';
+                echo '<a href="article_sorta.php?index=' . $index . '&naziv=' . urlencode($vino["naziv"]) . '&image=' . urlencode($vino["slika"]) . '&kratkiOpis=' . urlencode($vino["kratkiOpis"]) . '&duziOpis=' . urlencode($vino["duziOpis"]) . '&boja=' . urlencode($vino["boja"]) . '&slatkoca=' . urlencode($vino["slatkoca"]) . '&preporucenaJela=' . urlencode($vino["preporucenaJela"]) . '&date=' . urlencode($vino["datumObjavljivanja"]) . '">';
                 echo '<article class="card">';
                 echo '<img src="' . $vino["slika"] . '" alt="slika" />';
                 echo '<div class="article-text">';
